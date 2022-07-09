28a4f3b9699be54d221d0e48bef8ff68175629d2
Karishma6543 <76261120+Karishma6543@users.noreply.github.com>
2022-07-09 16:35:33 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,58 @@
+<?php include "header.php"; ?>	
+	<!-- Start All Pages -->
+		
+		<img src="images/cn.png" width="100%">
+	<!-- End All Pages -->
+	
+	<!-- Start Contact -->
+	<div>
+		<iframe src="https://goo.gl/maps/mTeF37hPDgcpimfN8" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+	</div>
+	<div class="contact-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Contact</h2>
+						<p>Contact Information are Listed here Thanks for Visit Our Site</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<form id="contactForm">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="form-group">
+									<h1 style="font-family: cambria; color: red; text-align: center;">K's Kitchen</h1>
+									<div class="help-block with-errors"></div>
+								</div>                                 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Email Us :</b> kskitchen@gmail.com</span></center>
+									<div class="help-block with-errors"></div>
+								</div> 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Mobile  :</b> +91 992 4042 799</span></center>
+									<div class="help-block with-errors"></div>
+								</div> 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group"> 
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Location  :</b> Dombivli, Mumbai, Maharashtra, Pin - 380001</span></center>
+									<div class="help-block with-errors"></div>
+								</div>
+								
+							</div>
+						</div>            
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Contact -->
+<?php include "footer.php"; ?>
